f43ab4f73969944e42b5dc219c2a55bcc99c9310
Aldian Fazrihady <mobile@aldian.net>
2020-03-29 09:24:15 +0700

    p1s3t1 create need

--- boots_main.py
+++ boots_main.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 import datetime
 import json
 import logging
-import os
 import sys
 import time
 
@@ -26,9 +25,8 @@ from GCP_datastore_logging import LoggingFuctions
 from p1_global_settings import GlobalSettings as GSB
 from p1_services import Services as Services
 from p1_services import TaskArguments
-from p1_services import  TaskNames
+from p1_services import TaskNames
 from p1_datastores import Datastores as DsP1
-from datastore_functions import DatastoreFunctions as DSF
 from task_queue_functions import CreateTransactionFunctions as CTF
 
 
@@ -149,7 +147,7 @@ class CommonPostHandler(DataValidation, OauthVerify):
 
     def post(self, *args, **kwargs):
         debug_data = []
-        task_id = 'create-transaction:CommonPostHandler:post'
+        task_id = 'web-requests:CommonPostHandler:post'
 
         self.response.headers[str('Access-Control-Allow-Headers')] = str(
             'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With')
@@ -168,8 +166,9 @@ class CommonPostHandler(DataValidation, OauthVerify):
             for key in self.request.arguments():
                 params[key] = self.request.get(key, None)
             LF = LoggingFuctions()
-            LF.logError(call_result[RDK.success],task_id,params,None,None,
-                        call_result[RDK.return_msg],call_result)
+            LF.logError(
+                call_result[RDK.success], task_id, params, None, None, call_result[RDK.return_msg], call_result
+            )
 
         self.create_response(call_result)
 
@@ -193,23 +192,23 @@ class CommonPostHandler(DataValidation, OauthVerify):
         self.response.out.write(call_result['return_msg'])
 
 
-@app.route(Services.web_request.create_user.url, methods=["OPTIONS", "POST"])
-@wrap_webapp_class(Services.web_request.create_user.name)
-class CreateUser(CommonPostHandler):
+@app.route(Services.web_request.create_need.url, methods=["OPTIONS", "POST"])
+@wrap_webapp_class(Services.web_request.create_need.name)
+class CreateNeed(CommonPostHandler):
     def process_request(self):
-        task_id = 'P3-CreateUser:process_request'
+        task_id = 'web-requests:CreateNeed:process_request'
         debug_data = []
-        return_msg = "P3-CreateUser:process_request "
+        return_msg = task_id + ": "
         user_uid = "1"
 
         # input validation
-        first_name = unicode(self.request.get(TaskArguments.s3t3_first_name, ""))
-        last_name = unicode(self.request.get(TaskArguments.s3t3_last_name, ""))
-        phone = unicode(self.request.get(TaskArguments.s3t3_phone_number, "")) or None
+        need_name = unicode(self.request.get(TaskArguments.s3t1_name, ""))
+        requirements = unicode(self.request.get(TaskArguments.s3t1_requirements, "")) or None
 
-        call_result = self.ruleCheck([[first_name,GSB.post_data_rules.required_name],
-                                     [last_name,GSB.post_data_rules.required_name],
-                                     [phone,DsP1.users._rule_phone_1]])
+        call_result = self.ruleCheck([
+            [need_name, DsP1.needs._rule_need_name],
+            [requirements, DsP1.needs._rule_requirements],
+        ])
 
         debug_data.append(call_result)
         if call_result['success'] != RC.success:
@@ -218,13 +217,17 @@ class CreateUser(CommonPostHandler):
         # </end> input validation
 
         ## create transaction to create user in datastore
-        task_sequence = [ {
-            'name': TaskNames.s1t4,
-            'PMA': {
-                TaskArguments.s1t4_first_name: unicode(first_name),
-                TaskArguments.s1t4_last_name: unicode(last_name),
-                TaskArguments.s1t4_phone_number: unicode(phone)
-            }
+        pma = {
+            TaskArguments.s1t1_name: need_name,
+        }
+        if requirements:
+            pma.update({
+                TaskArguments.s1t1_requirements: requirements,
+            })
+
+        task_sequence = [{
+            'name': TaskNames.s1t1,
+            'PMA': pma,
         }]
 
         try:
@@ -236,6 +239,66 @@ class CreateUser(CommonPostHandler):
         task_functions = CTF()
         call_result = task_functions.createTransaction(GSB.project_id, user_uid, task_id,
                                                        task_sequence)
+        debug_data.append(call_result)
+        if call_result['success'] != RC.success:
+            return_msg += 'failed to add task queue function'
+            return {'success': call_result['success'], 'debug_data': debug_data, 'return_msg': return_msg}
+        ##</end> create transaction to create user in datastore
+
+        return {'success': RC.success, 'return_msg': return_msg, 'debug_data': debug_data}
+
+
+@app.route(Services.web_request.create_user.url, methods=["OPTIONS", "POST"])
+@wrap_webapp_class(Services.web_request.create_user.name)
+class CreateUser(CommonPostHandler):
+    def process_request(self):
+        task_id = 'web-requests:CreateUser:process_request'
+        debug_data = []
+        return_msg = task_id + ": "
+        user_uid = "1"
+
+        # input validation
+        first_name = unicode(self.request.get(TaskArguments.s3t3_first_name, ""))
+        last_name = unicode(self.request.get(TaskArguments.s3t3_last_name, ""))
+        phone = unicode(self.request.get(TaskArguments.s3t3_phone_number, "")) or None
+
+        call_result = self.ruleCheck([
+            [first_name, GSB.post_data_rules.required_name],
+            [last_name, GSB.post_data_rules.required_name],
+            [phone, DsP1.users._rule_phone_1]
+        ])
+
+        debug_data.append(call_result)
+        if call_result['success'] != RC.success:
+            return_msg += "input validation failed"
+            return {'success': RC.input_validation_failed, 'return_msg': return_msg, 'debug_data': debug_data}
+        # </end> input validation
+
+        pma = {
+            TaskArguments.s1t4_first_name: first_name,
+            TaskArguments.s1t4_last_name: last_name,
+        }
+        if phone:
+            pma.update({
+                TaskArguments.s1t4_phone_number: unicode(phone)
+            })
+
+        ## create transaction to create user in datastore
+        task_sequence = [{
+            'name': TaskNames.s1t4,
+            'PMA': pma,
+        }]
+
+        try:
+            task_sequence = unicode(json.JSONEncoder().encode(task_sequence))
+        except Exception as e:
+            return_msg += "JSON encoding of task_queue failed with exception:%s" % e
+            return {'success': False, 'return_msg': return_msg, 'debug_data': debug_data}
+
+        task_functions = CTF()
+        call_result = task_functions.createTransaction(
+            GSB.project_id, user_uid, task_id, task_sequence
+        )
         debug_data.append(call_result)
         if call_result['success'] != RC.success:
             return_msg += 'failed to add task queue function'
